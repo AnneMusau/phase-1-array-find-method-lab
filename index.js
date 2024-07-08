2c275deed21ef64dbc07f119d3edbc86822b51e6
annemusau <moigeanne646@gmail.com>
2024-07-08 22:55:56 +0300

    array find method

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function superbowlWin(records) {
+    let winningRecord = records.find(record => record.result === "W")
+    return winningRecord ? winningRecord.year : undefined;
+}
+let superbowlRecords = [
+    { year: 2010, result: "L" },
+    { year: 2011, result: "W" },
+    { year: 2012, result: "L" },
+    { year: 2013, result: "L" }
+];
+
+console.log(superbowlWin(superbowlRecords)); // Output: 2011
+let noWinRecords = [
+    { year: 2010, result: "L" },
+    { year: 2011, result: "L" },
+    { year: 2012, result: "L" }
+];
+
+console.log(superbowlWin(noWinRecords)); // Output: undefined
